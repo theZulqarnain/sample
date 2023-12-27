7a84f38d3fd77237a42900ba2fb034b46d1ef94c
Abdulla Zulqarnain <zul@Abdullas-MacBook-Pro.local>
2023-12-27 11:45:56 +0530

    first commit

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/health', function(req, res, next) {
+  res.json({success:true,message:"server is up"})
+});
+
+module.exports = router;
